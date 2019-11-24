cb2331dce469ea9e80db185bec1552d655a682d5
TahsinMir <tahsin.imtiaz@northsouth.edu>
2019-11-24 13:57:59 -0700

    Ephemeral key added to greater than table

--- src/ElgamalTest/TestGreaterThanTable.java
+++ src/ElgamalTest/TestGreaterThanTable.java
@@ -28,24 +28,22 @@ public class TestGreaterThanTable {
         BigInteger zeroAlt = BigInteger.valueOf(50);    //just for test, assuming that we represent 0 with 50
         System.out.println("onr = " + one + " and negOne = " + negOne);
 
-        ElGamalMessage e;
-        BigInteger encOne, encNegOne, encZeroAlt;
+        ElGamalMessage encOne, encNegOne, encZeroAlt;
+        //BigInteger encOne, encNegOne, encZeroAlt;
         //System.out.println("starting encryption:");
 
-        e = ElGamal.encryptMessage(publicKey, one);
-        encOne = e.getEncryptedMessage();
+        encOne = ElGamal.encryptMessage(publicKey, one);
+        System.out.println("enc1 = " + encOne.getEncryptedMessage() + ", " + encOne.getEphimeralKey());
         //System.out.println("done1");
 
-        e = ElGamal.encryptMessage(publicKey, zeroAlt);
-        encZeroAlt = e.getEncryptedMessage();
+        encZeroAlt = ElGamal.encryptMessage(publicKey, zeroAlt);
+        System.out.println("enc0alt = " + encZeroAlt.getEncryptedMessage() + ", " + encZeroAlt.getEphimeralKey());
         //System.out.println("done2");
 
-        e = ElGamal.encryptMessage(publicKey, negOne);
-        System.out.println("test");
-        encNegOne = e.getEncryptedMessage();
+        encNegOne = ElGamal.encryptMessage(publicKey, negOne);
+        System.out.println("encNeg1 = " + encNegOne.getEncryptedMessage() + ", " + encNegOne.getEphimeralKey());
         //System.out.println("done3");
 
-        System.out.println("Encrypted values:\n" + encOne + "\n" + encNegOne + "\n" + encZeroAlt);
         System.out.println("starting table generation test:");
         GreaterThanFunction greaterThanFunction = new GreaterThanFunction(encOne, encNegOne, encZeroAlt, encZeroAlt);   //sign is equal
         greaterThanFunction.generateFullGreaterThanTable();
