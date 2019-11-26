b3c873b4a0ba1768c390f92c70073e0682fc9039
TahsinMir <tahsin.imtiaz@northsouth.edu>
2019-11-25 18:28:59 -0700

    Greater than function check with PET; has issues with PET

--- src/PET/CheckPET.java
+++ src/PET/CheckPET.java
@@ -10,6 +10,8 @@ public class CheckPET {
 
    public boolean checkEqualityOfTwoMessage(ElGamalMessage m1, ElGamalMessage m2, ElGamalPrivateKey privateKey){
 
+       System.out.println("PET parameters: " + ElGamal.decryptMessage(m1,privateKey) + ", " + ElGamal.decryptMessage(m2,privateKey));
+
         BigInteger alphaMessage1, betaMessage1,alphaMessage2, betaMessage2, divideAlpha, divideBeta, decryptmessage;
 
         // get alpha and beta for message 1
@@ -20,21 +22,39 @@ public class CheckPET {
         alphaMessage2 = m2.getEncryptedMessage();
         betaMessage2 = m2.getEphimeralKey();
 
-        System.out.println("first message == " + m1.getEncryptedMessage() + ", " + m1.getEphimeralKey());
-        System.out.println("second message == " + m2.getEncryptedMessage() + ", " + m2.getEphimeralKey());
+        //System.out.println("first message == " + m1.getEncryptedMessage() + ", " + m1.getEphimeralKey());
+        //System.out.println("second message == " + m2.getEncryptedMessage() + ", " + m2.getEphimeralKey());
         //Divide m1/m2
-        divideAlpha = alphaMessage1.divide(alphaMessage2);
-        divideBeta =betaMessage1.divide(betaMessage2);
+       if(alphaMessage1.compareTo(alphaMessage2) == 1)
+       {
+           divideAlpha = alphaMessage1.divide(alphaMessage2);
+           //divideBeta =betaMessage1.divide(betaMessage2);
+       }
+       else
+       {
+           divideAlpha = alphaMessage2.divide(alphaMessage1);
+           //divideBeta =betaMessage2.divide(betaMessage1);
+       }
+       if(betaMessage1.compareTo(betaMessage2) == 1)
+       {
+           divideBeta = betaMessage1.divide(betaMessage2);
+       }
+       else
+       {
+           divideBeta = betaMessage2.divide(betaMessage1);
+       }
 
-        System.out.println("divideAlpha == " + divideAlpha);
-       System.out.println("divideBeta == " + divideBeta);
+
+        //System.out.println("divideAlpha == " + divideAlpha);
+        //System.out.println("divideBeta == " + divideBeta);
 
         ElGamalMessage newelgamal=new ElGamalMessage(divideBeta, divideAlpha);
 
-
+        System.out.println("PET before decryption == " + newelgamal.getEncryptedMessage() + ", " + newelgamal.getEphimeralKey());
         //decreypt result
        decryptmessage=ElGamal.decryptMessage(newelgamal, privateKey);
-       System.out.println("decrypted message == " + decryptmessage);
+       System.out.println("pet total decryption = " + decryptmessage);
+       //System.out.println("decrypted message == " + decryptmessage);
        if (decryptmessage.equals(BigInteger.valueOf(1))){
            return true;
        }
